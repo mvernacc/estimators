273bf7345f502f56ed061dfe7552673adc4ca229
Matthew Vernacchia <mvernacc@mit.edu>
2015-12-04 02:35:06 -0500

    Track gyro bias in kraft_demo.py

--- kraft_demo.py
+++ kraft_demo.py
@@ -37,7 +37,8 @@ def main():
     dt=0.1
     # Create the sensors for the Kalman filter estimator (known bias parameters).
     magneto_est = Magnetometer(h_bias_ned=[0, 0, 0], h_bias_sensor=[0, 0, 0])
-    gyro_est = RateGyro(constant_bias=[0,0,0], bias_walk_process_std_dev=0,
+    magneto_est.is_stateful = False
+    gyro_est = RateGyro(constant_bias=[0,0,0],
         dt=dt)
 
     # System process noise covariance
@@ -47,13 +48,21 @@ def main():
 
     # Number of system states.
     n_system_states = 7
+    # Number of sensor bias states.
+    n_sensor_states = 3
+
 
     est_sensors = KalmanSensors([gyro_est, magneto_est],
-        [[4, 5, 6], [0, 1, 2, 3]], n_system_states)
+        [[4, 5, 6], [0, 1, 2, 3]], n_system_states,
+        [[7, 8, 9], []], n_sensor_states,
+        lambda x, u: rotation_dynamics(x, u, dt),
+        W,
+        1)
 
     # Create the sensors for the simulation (unknown, random bias parameters). 
     gyro_sim = RateGyro(dt=dt)
     magneto_sim = Magnetometer(h_bias_ned=[0, 0, 0], h_bias_sensor=[0, 0, 0])
+    magneto_sim.is_stateful = False
     sim_sensors = KalmanSensors([gyro_sim, magneto_sim],
         [[4, 5, 6], [0, 1, 2, 3]], n_system_states)
 
@@ -62,7 +71,8 @@ def main():
 
     # Initial state estimate. Set the sensor bias states
     # to an initial estimate of zero.
-    x_est_init = np.array([1., 0., 0., 0., 0., 0., 0.])
+    x_est_init = np.concatenate(([1., 0., 0., 0., 0., 0., 0.],
+        np.zeros(n_sensor_states)))
 
     # Initial estimate covariance.
     # The std. dev. uncertainty of the initial system state
@@ -70,15 +80,18 @@ def main():
     system_state_init_std_dev = np.hstack((np.deg2rad([1., 1., 1.]), 
         np.deg2rad([1., 1., 1.])))
     # The std. dev. uncertainty of the sensor bias states.
-    sensor_state_init_std_dev = []
-    Q_init = np.diag(system_state_init_std_dev)**2
+    sensor_state_init_std_dev = np.deg2rad([5., 5., 5.])
+    Q_init = np.diag(np.concatenate((
+        system_state_init_std_dev,
+        sensor_state_init_std_dev
+        )))**2
 
     # Create the Kalman Filter
     ukf = KraftQautUKF(
         x_est_init,
         Q_init,
-        lambda x, u: rotation_dynamics(x, u, dt),
-        W,
+        est_sensors.augmented_transition_function,
+        est_sensors.augmented_process_covariance,
         est_sensors.measurement_function,
         est_sensors.noise_cov,
         )
@@ -93,6 +106,7 @@ def main():
 
     y_traj = np.zeros((n_steps, len(sim_sensors.measurement_function(x_init))))
 
+    gyro_bias_traj = np.zeros((n_steps, 3))
     x_est = x_est_init
     Q = Q_init
 
@@ -101,6 +115,9 @@ def main():
         u_traj[i] = np.deg2rad([10.0, 0, 0])
 
     for i in xrange(1, n_steps):
+        # Update sensor state
+        sim_sensors.update_sensor_state(x_traj[i-1])
+        gyro_bias_traj[i] = gyro_sim.bias + gyro_sim.constant_bias
         # Get measurements.
         y_traj[i] = sim_sensors.add_noise(sim_sensors.measurement_function(x_traj[i-1]))
         # Update Kalman filter estimate.
@@ -118,7 +135,7 @@ def main():
     print 'Final estimate covariance Q = '
     print ukf.Q
 
-    ax = plt.subplot(3, 1, 1)
+    ax = plt.subplot(2, 2, 1)
     colors = ['black', 'red', 'green', 'blue']
     for i in xrange(4):
         plt.plot(t_traj, x_traj[:, i], color=colors[i], linestyle='-',
@@ -129,7 +146,7 @@ def main():
     plt.ylabel('Quaternion')
     plt.legend(framealpha=0.5)
     
-    ax2 = plt.subplot(3, 1, 2, sharex=ax)
+    ax2 = plt.subplot(2, 2, 2, sharex=ax)
     Q_traj_padded = np.concatenate((
         np.zeros((n_steps, len(x_est_init), 1)),
         np.concatenate((
@@ -146,12 +163,23 @@ def main():
     plt.ylabel('Angular rate [rad / s]')
     plt.legend(framealpha=0.5)
 
-    ax3 = plt.subplot(3, 1, 3, sharex=ax)
+    ax3 = plt.subplot(2, 2, 3, sharex=ax)
     for i in xrange(3):
         plt.plot(t_traj, y_traj[:, i + 3], color=colors[i+1], marker='x',
             label='mag[{:d}]'.format(i))
     plt.xlabel('Time [s]')
     plt.ylabel('Mag Field [uT]')
+    plt.legend(framealpha=0.5)
+
+    ax4 = plt.subplot(2, 2, 4, sharex=ax)
+    for i in [0, 1, 2]:
+        plt.plot(t_traj, gyro_bias_traj[:, i], color=colors[i+1], linestyle='-',
+            label='b[{:d}] true'.format(i))
+        plot_single_state_vs_time(ax4, t_traj, x_est_traj, Q_traj_padded, i+7,
+            color=colors[i+1], label='b[{:d}] est'.format(i),
+            linestyle='--')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Gyro bias [rad / s]')
     plt.legend(framealpha=0.5)
 
     plt.show()
